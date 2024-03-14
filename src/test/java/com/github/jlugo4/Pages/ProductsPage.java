4814030962f78e245097b241cfb3555f40d926ff
JLugo4 <jarek.lugo96@gmail.com>
2024-03-14 19:51:51 -0400

    productpage added

---
+++ src/test/java/com/github/jlugo4/Pages/ProductsPage.java
@@ -0,0 +1,12 @@
+package com.github.jlugo4.Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ProductsPage {
+
+    private WebDriver driver;
+    public ProductsPage(WebDriver driver){this.driver = driver;}
+
+    
+
+}

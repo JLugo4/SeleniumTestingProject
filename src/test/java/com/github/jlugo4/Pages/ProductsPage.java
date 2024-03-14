66e4f758ac509784f5a627d71cf1809607dad6e2
JLugo4 <jarek.lugo96@gmail.com>
2024-03-14 19:58:47 -0400

    Added methods to products page

--- src/test/java/com/github/jlugo4/Pages/ProductsPage.java
+++ src/test/java/com/github/jlugo4/Pages/ProductsPage.java
@@ -1,12 +1,32 @@
 package com.github.jlugo4.Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class ProductsPage {
 
     private WebDriver driver;
     public ProductsPage(WebDriver driver){this.driver = driver;}
 
+    //Search bar
+    final private By SearchBar = By.cssSelector("input[id*='search_product']");
+
+
+    //click Search bar
+    public void clickSearchProduct(){
+        driver.findElement(SearchBar).click();
+    }
+
+
+    // Search for sweater
+    public void searchForSweater(){
+        WebElement searchSweater = driver.findElement(SearchBar);
+        searchSweater.clear();
+        searchSweater.sendKeys("sweater");
+    }
+
     
+
 
 }

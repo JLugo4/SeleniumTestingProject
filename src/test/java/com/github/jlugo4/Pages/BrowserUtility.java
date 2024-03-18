d58715a815c1516174a715853e86b4fc366ed12e
JLugo4 <jarek.lugo96@gmail.com>
2024-03-18 18:31:21 -0400

    More comment clean up

--- src/test/java/com/github/jlugo4/Pages/BrowserUtility.java
+++ src/test/java/com/github/jlugo4/Pages/BrowserUtility.java
@@ -7,17 +7,23 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowserUtility {
+
+        //WebDriver
         private WebDriver driver;
+
+        //Initialize BrowserUtility WebDriver
         public BrowserUtility(WebDriver driver) {
             this.driver = driver;
         }
 
+        //Scroll down method
         public void scrollDown(int pixels) {
             JavascriptExecutor js = (JavascriptExecutor) driver;
             js.executeScript("window.scrollBy(0, " + pixels + ")");
         }
 
-    public void waitForElementToBeClickable(By locator, int timeoutInSeconds) {
+        // Wait Method
+        public void waitForElementToBeClickable(By locator, int timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.until(ExpectedConditions.elementToBeClickable(locator));
     }

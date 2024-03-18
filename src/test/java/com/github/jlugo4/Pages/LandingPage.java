cd7609acf9426ce7dea3dcbaddf7111308bc8c30
JLugo4 <jarek.lugo96@gmail.com>
2024-03-18 18:27:48 -0400

    More clean up

--- src/test/java/com/github/jlugo4/Pages/LandingPage.java
+++ src/test/java/com/github/jlugo4/Pages/LandingPage.java
@@ -5,18 +5,24 @@ import org.openqa.selenium.WebDriver;
 
 public class LandingPage {
 
+    //Initialize signUpLink
     private By signUpLink = By.cssSelector("a[href*='/login']");
 
+    //Initialize ProductsLink
     private By productsLink = By.cssSelector("a[href*='/products']");
 
+    // Initialize the WebDriver
     private WebDriver driver;
 
+    // Initialize LandingPage Web Driver
     public LandingPage(WebDriver driver){this.driver = driver;}
 
+    // click signUpLink
     public void clickSignUpLink(){
         driver.findElement(signUpLink).click();
     }
 
+    // click productsLink
     public void clickProducts(){
         driver.findElement(productsLink).click();
     }
